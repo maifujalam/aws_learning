d39c1e57459c9128304ada7144a7b230af76eab5
alam <sk.maifujalam@gmail.com>
2023-10-23 15:28:27 +0530

    s3

--- s3/public-helm-repository/main.tf
+++ s3/public-helm-repository/main.tf
@@ -1,5 +1,26 @@
 module "public-helm-repository" {
-  source = "../../modules/bucket"
+  source      = "../../modules/bucket"
   bucket_name = var.bucket_name
-  region = var.region
+  region      = var.region
+}
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = module.public-helm-repository.bucket_id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public-access-control" {
+  bucket                  = module.public-helm-repository.bucket_id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "public-access" {
+  depends_on = [aws_s3_bucket_ownership_controls.ownership, aws_s3_bucket_public_access_block.public-access-control]
+
+  bucket = module.public-helm-repository.bucket_id
+  acl    = "public-read"
 }
