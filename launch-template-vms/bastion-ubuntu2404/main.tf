c7be668796e82910935cf1ded77d62a3fd56b9cf
ALAM <sk.maifujalam@gmail.com>
2024-08-09 02:26:42 +0530

    df

--- launch-template-vms/bastion-ubuntu2404/main.tf
+++ launch-template-vms/bastion-ubuntu2404/main.tf
@@ -1,6 +1,5 @@
-
 module "bastion-ubuntu2404" {
-  source = "/home/alam/Desktop/Projects/aws_learning/modules/launch-template-vm"
+  source = "../../modules/launch-template-vm"
   region = var.region
   instance_name = var.instance_name
 }
